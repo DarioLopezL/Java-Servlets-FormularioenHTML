5e49ba6f70af45ec410fa88fd9dd49c07fdaefdf
DarioLopezL <dariiolopez17@gmail.com>
2021-09-09 19:53:10 -0500

    mi primer commit

---
+++ src/main/java/web/Servlet.java
@@ -0,0 +1,114 @@
+
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    String usuario = request.getParameter("usuario");
+    String password = request.getParameter("password");
+    String tecnologias[] = request.getParameterValues("tecnologia");
+    String genero = request.getParameter("genero");
+    String ocupacion = request.getParameter("ocupacion");
+    String musica[] = request.getParameterValues("musica");
+    String comentarios = request.getParameter("comentarios");
+    
+    out.print("<html>");
+    out.print("<head>");
+    out.print("<title>Resultado Servlet </title>");
+    out.print("</head>");
+    out.print("<body>");
+    out.print("<h1> parametros procesados por el servlet : </h1>");
+    out.print("<table border='1'>");
+    
+    out.print("<tr>");
+    out.print("<td>");
+    out.print("Usuario");
+    out.print("</td>");
+    out.print("<td>");
+    out.print(usuario);
+    out.print("</td>");
+    out.print("</tr>");
+    
+    out.print("<tr>");
+    out.print("<td>");
+    out.print("Password");
+    out.print("</td>");
+    out.print("<td>");
+    out.print(password);
+    out.print("</td>");
+    out.print("</tr>");
+    
+    out.print("<tr>");
+    out.print("<td>");
+    out.print("Tecnologias");
+    out.print("</td>");
+    out.print("<td>");
+      for(String tecnologia: tecnologias){
+    out.print(tecnologia);
+    out.print(" / ");
+    }
+    out.print("</td>");
+    out.print("</tr>");
+    
+    out.print("<tr>");
+    out.print("<td>");
+    out.print("Genero");
+    out.print("</td>");
+    out.print("<td>");
+    out.print(genero);
+    out.print("</td>");
+    out.print("</tr>");
+    
+    out.print("<tr>");
+    out.print("<td>");
+    out.print("Ocupacion");
+    out.print("</td>");
+    out.print("<td>");
+    out.print(ocupacion);
+    out.print("</td>");
+    out.print("</tr>");
+    
+    out.print("<tr>");
+    out.print("<td>");
+    out.print("Musica Favorita");
+    out.print("</td>");
+    out.print("<td>");
+    if(musica != null){
+    for(String music: musica){
+    out.print(music);
+    out.print(" / ");
+    }
+    }
+    else{
+            out.print("Valor no proporcionado");
+            }
+    out.print("</td>");
+    out.print("</tr>");
+    
+    out.print("<tr>");
+    out.print("<td>");
+    out.print("Comentarios");
+    out.print("</td>");
+    out.print("<td>");
+    out.print(comentarios);
+    out.print("</td>");
+    out.print("</tr>");
+ 
+   
+    
+    out.print("</table>");
+    out.print("</body>");
+    out.print("</html>");
+    
+    }
+    
+}
---
+++ src/main/webapp/recursos/funciones.js
@@ -0,0 +1,47 @@
+function validarForma(forma) {
+    var usuario = forma.usuario;
+    if (usuario.value=="" || usuario.value == "Escribir usuario") {
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        return false;
+    }
+    var password = forma.password;
+    if (password.value == "" || password.value.length < 3) {
+        alert("Debe proporcionar un password al menos de 3 caracteres ");
+        password.focus();
+        password.select();
+        return false;
+    }
+    var tecnologias = forma.tecnologia;
+    var checkSeleccionado = false;
+    for (var i = 0; i < tecnologias.length; i++) {
+        if (tecnologias[i].checked) {
+            checkSeleccionado = true;
+        }
+    }
+    if(!checkSeleccionado){
+        alert("Debe seleccionar una tecnologia");        
+        return false;
+    }
+    var generos = forma.genero;
+    var radioSeleccionado = false;
+    for(var i=0;i< generos.length; i++){
+        if(generos[i].checked){
+            radioSeleccionado = true;
+        }
+    }
+    if(!radioSeleccionado){
+        alert("Debe seleccionar un genero");
+        return false;
+    }
+    var ocupacion = forma.ocupacion;
+    if(ocupacion.value == ""){
+        alert("Debe seleccionar una ocupacion");
+        return false;
+    }
+    alert("Formulario valido, enviando Datos al servidor ");
+    return true;
+
+}
+
